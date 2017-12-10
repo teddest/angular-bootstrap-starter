502b280d235409de871f041218ab346a58f55311
Tedd Van Diest <tedd.vandiest@gmail.com>
2017-12-10 15:34:47 -0500

    feat(style): Add bootstrap (s)css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PublicComponent } from './public/public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/public', pathMatch: 'full' },
+  { path: 'public', component: PublicComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,31 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        RouterTestingModule
+      ]
+    });
+    TestBed.compileComponents();
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it(`should have as title 'Angular Bootstrap starter template'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Angular Bootstrap starter template');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
 });
