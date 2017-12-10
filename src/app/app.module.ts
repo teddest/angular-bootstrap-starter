f87c71e5acb458dc4bc44c8e573a7928b1764d7d
Tedd Van Diest <tedd.vandiest@gmail.com>
2017-12-10 15:34:47 -0500

    feat(style): Add bootstrap (s)css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/public/public.component.spec.ts
@@ -0,0 +1,43 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { PublicComponent } from './public.component';
+
+describe('PublicComponent', () => {
+  let component: PublicComponent;
+  let fixture: ComponentFixture<PublicComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        PublicComponent
+      ],
+      imports: [
+        RouterTestingModule,
+        NgbModule.forRoot()
+      ]
+    });
+    TestBed.compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PublicComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the carousel component', () => {
+    fixture = TestBed.createComponent(PublicComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const qry = compiled.querySelector('ngb-carousel').textContent;
+    expect(qry).not.toBeNull();
+  });
+
+});
