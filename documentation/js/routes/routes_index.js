14a6bc210930f31b4620599bf610e5e3bf9ab7c6
teddest <tedd@tedd.us>
2019-03-01 15:32:33 -0500

    chore(doc): Add compodoc

---
+++ documentation/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"routes","filename":"src/app/app-routing.module.ts","module":"AppRoutingModule","children":[{"path":"","redirectTo":"/public","pathMatch":"full"},{"path":"public","component":"PublicComponent"}],"kind":"module"}]}
